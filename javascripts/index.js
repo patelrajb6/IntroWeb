0fa84c279121c651bfdb60932f70463f0a2dcd14
Raj Patel <raj.patel@ymail.com>
2020-05-20 02:12:39 -0500

    message

---
+++ javascripts/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const nav_bar= document.querySelector("navigation_bar")
+
+ReactDOM.render(React.createElement(
+    "p","Learning React",nav_bar
+));
